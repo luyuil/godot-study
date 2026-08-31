24dd029b8468ffb3982cf42dc457185e0e2dabea
luyuil <19057235886@163.com>
2026-08-31 23:03:16 +0800

    开始做一个过马路游戏

---
+++ 过马路/scenes/decoration.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+func _physics_process(_delta: float) -> void:
+	scale = scale * 1.001
+	
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
---
+++ 过马路/scenes/player.gd
@@ -0,0 +1,21 @@
+extends Sprite2D
+
+
+var direction: Vector2 = Vector2.ZERO
+var speed: int = 5
+
+func _physics_process(_delta: float) -> void:
+	direction = Input.get_vector("left","right","up","down")
+	position += direction * speed
+	
+	if Input.is_action_just_pressed("confirm"):
+		print("start")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
